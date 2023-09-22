6052e640f1a71004317fc8690b0bcc624d3931e3
Osvaldo Hernandez <osvaldo.hernandez@ticdefense.global>
2023-09-21 19:27:11 -0600

    Se agregaron o modificaron los siguientes archivos: UpdateRepostories/UpdateRepos.ps1

--- UpdateRepostories/UpdateRepos.ps1
+++ UpdateRepostories/UpdateRepos.ps1
@@ -1,33 +1,41 @@
-# Ruta de la carpeta principal que contiene los repositorios
-$carpetaRepositorios = "D:\osvaldohm\Desktop\Repositories"
+# Lista de carpetas con repositorios
+$carpetasRepositorios = @(
+    "D:\osvaldohm\Desktop\Repositories"
+    # Agrega aquí más carpetas si es necesario
+)
 
-# Obtiene una lista de las subcarpetas (que representan los repositorios)
-$repositorios = Get-ChildItem -Path $carpetaRepositorios -Directory
+# Bucle a través de cada carpeta de repositorio
+foreach ($carpetaRepositorio in $carpetasRepositorios) {
+    # Obtiene una lista de las subcarpetas (que representan los repositorios)
+    $repositorios = Get-ChildItem -Path $carpetaRepositorio -Directory
 
-# Bucle a través de cada repositorio
-foreach ($repositorio in $repositorios) {
-    # Verifica si el directorio actual es un repositorio Git
-    if (Test-Path -Path (Join-Path -Path $repositorio.FullName -ChildPath ".git") -PathType Container) {
-        # Cambia el directorio de trabajo al repositorio actual
-        Set-Location -Path $repositorio.FullName
+    # Bucle a través de cada repositorio en la carpeta actual
+    foreach ($repositorio in $repositorios) {
+        # Verifica si el directorio actual es un repositorio Git
+        if (Test-Path -Path (Join-Path -Path $repositorio.FullName -ChildPath ".git") -PathType Container) {
+            # Cambia el directorio de trabajo al repositorio actual
+            Set-Location -Path $repositorio.FullName
+            
+            git pull origin
 
-        # Agregar todos los cambios
-        git add .
+            # Agregar todos los cambios
+            git add .
 
-        # Obtener los archivos que se han agregado al commit
-        $archivosAgregados = git diff --staged --name-only
+            # Obtener los archivos que se han agregado al commit
+            $archivosAgregados = git diff --staged --name-only
 
-        # Generar un mensaje de commit basado en los archivos agregados
-        $mensajeCommit = "Se agregaron los siguientes archivos:`n$archivosAgregados"
+            # Generar un mensaje de commit basado en los archivos agregados
+            $mensajeCommit = "Se agregaron o modificaron los siguientes archivos:`n$archivosAgregados"
 
-        # Realizar un commit con el mensaje generado
-        git commit -m $mensajeCommit
+            # Realizar un commit con el mensaje generado
+            git commit -m $mensajeCommit
 
-        # Hacer push a la rama origin (asegúrate de tener una rama llamada 'origin' configurada)
-        git push origin
+            # Hacer push a la rama origin (asegúrate de tener una rama llamada 'origin' configurada)
+            git push origin
 
-        # Volver al directorio principal de repositorios
-        Set-Location -Path $carpetaRepositorios
+            # Volver al directorio principal de repositorios
+            Set-Location -Path $carpetaRepositorio
+        }
     }
 }
 
